cacc11bcec80a8f3ad309ce76bb1e45753255a50
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-06-15 18:10:31 +0800

    done

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -4,6 +4,7 @@ package options;
 import Discord.DiscordClient;
 #end
 import flash.text.TextField;
+import flixel.addons.transition.FlxTransitionableState;
 import flixel.FlxG;
 import flixel.addons.display.FlxBackdrop;
 import flixel.FlxSprite;
