da7ced9367cbc674ab6a8cdf2f65a24ea9d22d13
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-06-15 16:55:56 +0800

    Add files via upload

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -4,7 +4,6 @@ package options;
 import Discord.DiscordClient;
 #end
 import flash.text.TextField;
-import flixel.addons.display.FlxBackdrop;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.addons.display.FlxGridOverlay;
@@ -25,9 +24,6 @@ import flixel.util.FlxTimer;
 import flixel.input.keyboard.FlxKey;
 import flixel.graphics.FlxGraphic;
 import Controls;
-import openfl.filters.ShaderFilter;
-import Shaders;
-import openfl.display.Shader;
 
 using StringTools;
 
@@ -41,41 +37,35 @@ class BaseOptionsMenu extends MusicBeatSubstate
 	private var checkboxGroup:FlxTypedGroup<CheckboxThingie>;
 	private var grpTexts:FlxTypedGroup<AttachedText>;
 
+	private var boyfriend:Character = null;
 	private var descBox:FlxSprite;
 	private var descText:FlxText;
 
 	public var title:String;
 	public var rpcTitle:String;
 
-	var starFG:FlxBackdrop;
-	var starBG:FlxBackdrop;
-
 	public function new()
 	{
 		super();
-
-		if(title == null) title = 'Options';
-		if(rpcTitle == null) rpcTitle = 'Options Menu';
 		
+		#if android
+		addVirtualPad(UP_DOWN, A_B);
+		#end
+
+		if (title == null)
+			title = 'Options';
+		if (rpcTitle == null)
+			rpcTitle = 'Options Menu';
+
 		#if desktop
 		DiscordClient.changePresence(rpcTitle, null);
 		#end
 
-		var shader:Shaders.ChromaticAberrationEffect = new Shaders.ChromaticAberrationEffect();
-		shader.setChrome(0.003);
-		FlxG.camera.setFilters([new ShaderFilter(shader.shader)]);
-		
-		var titlebg:FlxSprite = new FlxSprite(0, 0).makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
-		titlebg.scrollFactor.set();
-		add(titlebg);
-
-		starFG = new FlxBackdrop(Paths.image('mainmenu/mainmenuV4/starFG'), XY);
-		starFG.antialiasing = ClientPrefs.globalAntialiasing;
-		add(starFG);
-
-		starBG = new FlxBackdrop(Paths.image('mainmenu/mainmenuV4/starBG'), XY);
-		starBG.antialiasing = ClientPrefs.globalAntialiasing;
-		add(starBG);
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.color = 0xFFea71fd;
+		bg.screenCenter();
+		bg.antialiasing = ClientPrefs.globalAntialiasing;
+		add(bg);
 
 		// avoids lagspikes while scrolling through menus!
 		grpOptions = new FlxTypedGroup<Alphabet>();
@@ -108,19 +98,22 @@ class BaseOptionsMenu extends MusicBeatSubstate
 			var optionText:Alphabet = new Alphabet(290, 260, optionsArray[i].name, false);
 			optionText.isMenuItem = true;
 			/*optionText.forceX = 300;
-			optionText.yMult = 90;*/
+				optionText.yMult = 90; */
 			optionText.targetY = i;
 			grpOptions.add(optionText);
 
-			if(optionsArray[i].type == 'bool') {
+			if (optionsArray[i].type == 'bool')
+			{
 				var checkbox:CheckboxThingie = new CheckboxThingie(optionText.x - 105, optionText.y, optionsArray[i].getValue() == true);
 				checkbox.sprTracker = optionText;
 				checkbox.ID = i;
 				checkboxGroup.add(checkbox);
-			} else {
+			}
+			else
+			{
 				optionText.x -= 80;
 				optionText.startPosition.x -= 80;
-				//optionText.xAdd -= 80;
+				// optionText.xAdd -= 80;
 				var valueText:AttachedText = new AttachedText('' + optionsArray[i].getValue(), optionText.width + 80);
 				valueText.sprTracker = optionText;
 				valueText.copyAlpha = true;
@@ -128,7 +121,12 @@ class BaseOptionsMenu extends MusicBeatSubstate
 				grpTexts.add(valueText);
 				optionsArray[i].setChild(valueText);
 			}
-			//optionText.snapToPosition(); //Don't ignore me when i ask for not making a fucking pull request to uncomment this line ok
+			// optionText.snapToPosition(); //Don't ignore me when i ask for not making a fucking pull request to uncomment this line ok
+
+			if (optionsArray[i].showBoyfriend && boyfriend == null)
+			{
+				reloadBoyfriend();
+			}
 			updateTextFrom(optionsArray[i]);
 		}
 
@@ -136,20 +134,19 @@ class BaseOptionsMenu extends MusicBeatSubstate
 		reloadCheckboxes();
 	}
 
-	public function addOption(option:Option) {
-		if(optionsArray == null || optionsArray.length < 1) optionsArray = [];
+	public function addOption(option:Option)
+	{
+		if (optionsArray == null || optionsArray.length < 1)
+			optionsArray = [];
 		optionsArray.push(option);
 	}
 
 	var nextAccept:Int = 5;
 	var holdTime:Float = 0;
 	var holdValue:Float = 0;
+
 	override function update(elapsed:Float)
 	{
-
-        starFG.x -= 0.03;
-		starBG.x -= 0.01;
-
 		if (controls.UI_UP_P)
 		{
 			changeSelection(-1);
@@ -159,46 +156,55 @@ class BaseOptionsMenu extends MusicBeatSubstate
 			changeSelection(1);
 		}
 
-		if (controls.BACK) {
+		if (controls.BACK)
+		{
 			close();
 			FlxG.sound.play(Paths.sound('cancelMenu'));
 		}
 
-		if(nextAccept <= 0)
+		if (nextAccept <= 0)
 		{
 			var usesCheckbox = true;
-			if(curOption.type != 'bool')
+			if (curOption.type != 'bool')
 			{
 				usesCheckbox = false;
 			}
 
-			if(usesCheckbox)
+			if (usesCheckbox)
 			{
-				if(controls.ACCEPT)
+				if (controls.ACCEPT)
 				{
 					FlxG.sound.play(Paths.sound('scrollMenu'));
 					curOption.setValue((curOption.getValue() == true) ? false : true);
 					curOption.change();
 					reloadCheckboxes();
 				}
-			} else {
-				if(controls.UI_LEFT || controls.UI_RIGHT) {
+			}
+			else
+			{
+				if (controls.UI_LEFT || controls.UI_RIGHT)
+				{
 					var pressed = (controls.UI_LEFT_P || controls.UI_RIGHT_P);
-					if(holdTime > 0.5 || pressed) {
-						if(pressed) {
+					if (holdTime > 0.5 || pressed)
+					{
+						if (pressed)
+						{
 							var add:Dynamic = null;
-							if(curOption.type != 'string') {
+							if (curOption.type != 'string')
+							{
 								add = controls.UI_LEFT ? -curOption.changeValue : curOption.changeValue;
 							}
 
-							switch(curOption.type)
+							switch (curOption.type)
 							{
 								case 'int' | 'float' | 'percent':
 									holdValue = curOption.getValue() + add;
-									if(holdValue < curOption.minValue) holdValue = curOption.minValue;
-									else if (holdValue > curOption.maxValue) holdValue = curOption.maxValue;
+									if (holdValue < curOption.minValue)
+										holdValue = curOption.minValue;
+									else if (holdValue > curOption.maxValue)
+										holdValue = curOption.maxValue;
 
-									switch(curOption.type)
+									switch (curOption.type)
 									{
 										case 'int':
 											holdValue = Math.round(holdValue);
@@ -210,33 +216,42 @@ class BaseOptionsMenu extends MusicBeatSubstate
 									}
 
 								case 'string':
-									var num:Int = curOption.curOption; //lol
-									if(controls.UI_LEFT_P) --num;
-									else num++;
+									var num:Int = curOption.curOption; // lol
+									if (controls.UI_LEFT_P)
+										--num;
+									else
+										num++;
 
-									if(num < 0) {
+									if (num < 0)
+									{
 										num = curOption.options.length - 1;
-									} else if(num >= curOption.options.length) {
+									}
+									else if (num >= curOption.options.length)
+									{
 										num = 0;
 									}
 
 									curOption.curOption = num;
-									curOption.setValue(curOption.options[num]); //lol
-									//trace(curOption.options[num]);
+									curOption.setValue(curOption.options[num]); // lol
+									// trace(curOption.options[num]);
 							}
 							updateTextFrom(curOption);
 							curOption.change();
 							FlxG.sound.play(Paths.sound('scrollMenu'));
-						} else if(curOption.type != 'string') {
+						}
+						else if (curOption.type != 'string')
+						{
 							holdValue += curOption.scrollSpeed * elapsed * (controls.UI_LEFT ? -1 : 1);
-							if(holdValue < curOption.minValue) holdValue = curOption.minValue;
-							else if (holdValue > curOption.maxValue) holdValue = curOption.maxValue;
+							if (holdValue < curOption.minValue)
+								holdValue = curOption.minValue;
+							else if (holdValue > curOption.maxValue)
+								holdValue = curOption.maxValue;
 
-							switch(curOption.type)
+							switch (curOption.type)
 							{
 								case 'int':
 									curOption.setValue(Math.round(holdValue));
-								
+
 								case 'float' | 'percent':
 									curOption.setValue(FlxMath.roundDecimal(holdValue, curOption.decimals));
 							}
@@ -245,23 +260,26 @@ class BaseOptionsMenu extends MusicBeatSubstate
 						}
 					}
 
-					if(curOption.type != 'string') {
+					if (curOption.type != 'string')
+					{
 						holdTime += elapsed;
 					}
-				} else if(controls.UI_LEFT_R || controls.UI_RIGHT_R) {
+				}
+				else if (controls.UI_LEFT_R || controls.UI_RIGHT_R)
+				{
 					clearHold();
 				}
 			}
 
-			if(controls.RESET)
+			if (controls.RESET)
 			{
 				for (i in 0...optionsArray.length)
 				{
 					var leOption:Option = optionsArray[i];
 					leOption.setValue(leOption.defaultValue);
-					if(leOption.type != 'bool')
+					if (leOption.type != 'bool')
 					{
-						if(leOption.type == 'string')
+						if (leOption.type == 'string')
 						{
 							leOption.curOption = leOption.options.indexOf(leOption.getValue());
 						}
@@ -274,28 +292,37 @@ class BaseOptionsMenu extends MusicBeatSubstate
 			}
 		}
 
-		if(nextAccept > 0) {
+		if (boyfriend != null && boyfriend.animation.curAnim.finished)
+		{
+			boyfriend.dance();
+		}
+
+		if (nextAccept > 0)
+		{
 			nextAccept -= 1;
 		}
 		super.update(elapsed);
 	}
 
-	function updateTextFrom(option:Option) {
+	function updateTextFrom(option:Option)
+	{
 		var text:String = option.displayFormat;
 		var val:Dynamic = option.getValue();
-		if(option.type == 'percent') val *= 100;
+		if (option.type == 'percent')
+			val *= 100;
 		var def:Dynamic = option.defaultValue;
 		option.text = text.replace('%v', val).replace('%d', def);
 	}
 
 	function clearHold()
 	{
-		if(holdTime > 0.5) {
+		if (holdTime > 0.5)
+		{
 			FlxG.sound.play(Paths.sound('scrollMenu'));
 		}
 		holdTime = 0;
 	}
-	
+
 	function changeSelection(change:Int = 0)
 	{
 		curSelected += change;
@@ -310,18 +337,22 @@ class BaseOptionsMenu extends MusicBeatSubstate
 
 		var bullShit:Int = 0;
 
-		for (item in grpOptions.members) {
+		for (item in grpOptions.members)
+		{
 			item.targetY = bullShit - curSelected;
 			bullShit++;
 
 			item.alpha = 0.6;
-			if (item.targetY == 0) {
+			if (item.targetY == 0)
+			{
 				item.alpha = 1;
 			}
 		}
-		for (text in grpTexts) {
+		for (text in grpTexts)
+		{
 			text.alpha = 0.6;
-			if(text.ID == curSelected) {
+			if (text.ID == curSelected)
+			{
 				text.alpha = 1;
 			}
 		}
@@ -330,12 +361,37 @@ class BaseOptionsMenu extends MusicBeatSubstate
 		descBox.setGraphicSize(Std.int(descText.width + 20), Std.int(descText.height + 25));
 		descBox.updateHitbox();
 
-		curOption = optionsArray[curSelected]; //shorter lol
+		if (boyfriend != null)
+		{
+			boyfriend.visible = optionsArray[curSelected].showBoyfriend;
+		}
+		curOption = optionsArray[curSelected]; // shorter lol
 		FlxG.sound.play(Paths.sound('scrollMenu'));
 	}
 
-	function reloadCheckboxes() {
-		for (checkbox in checkboxGroup) {
+	public function reloadBoyfriend()
+	{
+		var wasVisible:Bool = false;
+		if (boyfriend != null)
+		{
+			wasVisible = boyfriend.visible;
+			boyfriend.kill();
+			remove(boyfriend);
+			boyfriend.destroy();
+		}
+
+		boyfriend = new Character(840, 170, 'bf', true);
+		boyfriend.setGraphicSize(Std.int(boyfriend.width * 0.75));
+		boyfriend.updateHitbox();
+		boyfriend.dance();
+		insert(1, boyfriend);
+		boyfriend.visible = wasVisible;
+	}
+
+	function reloadCheckboxes()
+	{
+		for (checkbox in checkboxGroup)
+		{
 			checkbox.daValue = (optionsArray[checkbox.ID].getValue() == true);
 		}
 	}
--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -5,7 +5,6 @@ import Discord.DiscordClient;
 #end
 import flash.text.TextField;
 import flixel.FlxG;
-import flixel.addons.display.FlxBackdrop;
 import flixel.FlxSprite;
 import flixel.addons.display.FlxGridOverlay;
 import flixel.group.FlxGroup.FlxTypedGroup;
@@ -14,14 +13,10 @@ import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import lime.utils.Assets;
 import flixel.FlxSubState;
-import lime.app.Application;
 import flash.text.TextField;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.util.FlxSave;
-import openfl.filters.ShaderFilter;
-import Shaders;
-import openfl.display.Shader;
 import haxe.Json;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
@@ -34,67 +29,72 @@ using StringTools;
 
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Controls', 'Adjust Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay'];
+	var options:Array<String> = [
+		'Note Colors',
+		'Controls',
+		'Adjust Delay and Combo',
+		'Graphics',
+		'Visuals and UI',
+		'Gameplay'
+	];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
+
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;
 
-	function openSelectedSubstate(label:String) {
-		switch(label) {
+	function openSelectedSubstate(label:String)
+	{
+		switch (label)
+		{
+			case 'Note Colors':
+			#if android
+				removeVirtualPad();
+				#end
+				openSubState(new options.NotesSubState());
 			case 'Controls':
+			#if android
+				removeVirtualPad();
+				#end
 				openSubState(new options.ControlsSubState());
-				Application.current.window.title = "Novisor Funkin' - Control Settings";
 			case 'Graphics':
+			#if android
+				removeVirtualPad();
+				#end
 				openSubState(new options.GraphicsSettingsSubState());
-				Application.current.window.title = "Novisor Funkin' - Graphic Settings";
 			case 'Visuals and UI':
+			#if android
+				removeVirtualPad();
+				#end
 				openSubState(new options.VisualsUISubState());
-				Application.current.window.title = "Novisor Funkin' - Visual and  UI Settings";
 			case 'Gameplay':
+			#if android
+				removeVirtualPad();
+				#end
 				openSubState(new options.GameplaySettingsSubState());
-				Application.current.window.title = "Novisor Funkin' - Gameplay Settings";
 			case 'Adjust Delay and Combo':
+			#if android
+				removeVirtualPad();
+				#end
 				LoadingState.loadAndSwitchState(new options.NoteOffsetState());
-				Application.current.window.title = "Novisor Funkin' - Adjust Delay & Combo Settings";
 		}
 	}
 
 	var selectorLeft:Alphabet;
 	var selectorRight:Alphabet;
 
-	var starFG:FlxBackdrop;
-	var starBG:FlxBackdrop;
-
-	override function create() {
+	override function create()
+	{
 		#if desktop
 		DiscordClient.changePresence("Options Menu", null);
 		#end
 
-		var shader:Shaders.ChromaticAberrationEffect = new Shaders.ChromaticAberrationEffect();
-		shader.setChrome(0.003);
-		FlxG.camera.setFilters([new ShaderFilter(shader.shader)]);
-
 		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
 		bg.color = 0xFFea71fd;
-		//bg.updateHitbox();
+		bg.updateHitbox();
 
 		bg.screenCenter();
 		bg.antialiasing = ClientPrefs.globalAntialiasing;
-		//add(bg);
-
-		var titlebg:FlxSprite = new FlxSprite(0, 0).makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
-		titlebg.scrollFactor.set();
-		add(titlebg);
-
-		starFG = new FlxBackdrop(Paths.image('mainmenu/mainmenuV4/starFG'), XY);
-		starFG.antialiasing = ClientPrefs.globalAntialiasing;
-		add(starFG);
-
-		starBG = new FlxBackdrop(Paths.image('mainmenu/mainmenuV4/starBG'), XY);
-		starBG.antialiasing = ClientPrefs.globalAntialiasing;
-		add(starBG);
-
-		Application.current.window.title = "Novisor Funkin' - Options Menu";
+		add(bg);
 
 		grpOptions = new FlxTypedGroup<Alphabet>();
 		add(grpOptions);
@@ -114,39 +114,59 @@ class OptionsState extends MusicBeatState
 
 		changeSelection();
 		ClientPrefs.saveSettings();
+		
+		#if android
+		addVirtualPad(UP_DOWN, A_B_X_Y);
+		#end
 
 		super.create();
 	}
 
-	override function closeSubState() {
+	override function closeSubState()
+	{
 		super.closeSubState();
 		ClientPrefs.saveSettings();
 	}
 
-	override function update(elapsed:Float) {
+	override function update(elapsed:Float)
+	{
 		super.update(elapsed);
 
-		starFG.x -= 0.03;
-		starBG.x -= 0.01;
-
-		if (controls.UI_UP_P) {
+		if (controls.UI_UP_P)
+		{
 			changeSelection(-1);
 		}
-		if (controls.UI_DOWN_P) {
+		if (controls.UI_DOWN_P)
+		{
 			changeSelection(1);
 		}
+		
+		#if android
+		if (_virtualpad.buttonX.justPressed) {
+			FlxTransitionableState.skipNextTransIn = true;
+			FlxTransitionableState.skipNextTransOut = true;
+			MusicBeatState.switchState(new android.AndroidControlsMenu());
+		}
+		if (_virtualpad.buttonY.justPressed) {
+			removeVirtualPad();
+			openSubState(new android.HitboxSettingsSubState());
+		}
+		#end
 
-		if (controls.BACK) {
+		if (controls.BACK)
+		{
 			FlxG.sound.play(Paths.sound('cancelMenu'));
 			MusicBeatState.switchState(new MainMenuState());
 		}
 
-		if (controls.ACCEPT) {
+		if (controls.ACCEPT)
+		{
 			openSelectedSubstate(options[curSelected]);
 		}
 	}
-	
-	function changeSelection(change:Int = 0) {
+
+	function changeSelection(change:Int = 0)
+	{
 		curSelected += change;
 		if (curSelected < 0)
 			curSelected = options.length - 1;
@@ -155,12 +175,14 @@ class OptionsState extends MusicBeatState
 
 		var bullShit:Int = 0;
 
-		for (item in grpOptions.members) {
+		for (item in grpOptions.members)
+		{
 			item.targetY = bullShit - curSelected;
 			bullShit++;
 
 			item.alpha = 0.6;
-			if (item.targetY == 0) {
+			if (item.targetY == 0)
+			{
 				item.alpha = 1;
 				selectorLeft.x = item.x - 63;
 				selectorLeft.y = item.y;
