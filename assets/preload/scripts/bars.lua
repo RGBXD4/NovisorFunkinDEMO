4ae5bd598b26a56888e35bf16dd82e6d0b02b536
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-06-15 15:39:20 +0800

    Update bars.lua

--- assets/preload/scripts/bars.lua
+++ assets/preload/scripts/bars.lua
@@ -1,10 +1,1 @@
-types = { 'normal', '480p', 'cover' }
 
-function onCreate()
-	for i = 1, #types do
-		if pcall(function() getTextFromFile('data/'..string.lower(songName)..'/songinfo'..(types[i] == 'normal' and '' or types[i])..'/songname.txt') end) then
-			addLuaScript('scripts/bars_type/'..types[i])
-			break
-		end
-	end
-end
